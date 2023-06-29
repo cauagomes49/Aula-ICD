9c80fefa82a08211262d4e81971707669821623b
cauagomes49 <cauangomes49@gmail.com>
2023-06-29 14:49:58 -0300

    Aula

---
+++ iris.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+
+iris = pd.read_csv('iris.data', header=None)
+iris = iris.to_numpy()
+
+X = iris[:,:-1]
+y = iris[:, -1]
+
+print(X.shape)
+
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X,y)
+
+clf.predict([[2,2,3,1]])
+
+y_estimado =[]
+for flor in X:
+    y_estimado.append(clf.predict([flor])[0])
